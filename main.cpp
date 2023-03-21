d02f459ff1316049e3bbd08325d3bdb97fec9323
Luca Denti <ldenti92@gmail.com>
2023-03-21 15:20:41 +0100

    Refactor code and remove second assembly round

--- main.cpp
+++ main.cpp
@@ -32,6 +32,103 @@ static const char RCN[128] = {
 
 using namespace std;
 
+void assemble(const vector<pair<int, int>> &specifics,
+              vector<pair<int, int>> &assembled_specifics) {
+  int i = specifics.size() - 1;
+  while (i >= 0) {
+    int j;
+    for (j = i - 1; j >= 0; --j) {
+      if (specifics[j + 1].second < specifics[j].first) {
+        // non-overlapping
+        assembled_specifics.push_back(
+            make_pair(specifics[i].first, specifics[j + 1].second));
+        i = j;
+        break;
+      }
+    }
+    if (j < 0) {
+      assembled_specifics.push_back(
+          make_pair(specifics[i].first, specifics[0].second));
+      i = j;
+    }
+  }
+}
+
+void query(gcsa::GCSA index, kseq_t *seq, int flank,
+           vector<pair<int, int>> &solutions) {
+  cerr << "Querying " << seq->name.s << ".." << endl;
+  gcsa::range_type range;
+  vector<gcsa::node_type> results;
+  vector<pair<int, int>> specifics;
+  int beg = seq->seq.l - 1;
+  int end = beg;
+  int p;
+  while (beg >= 0) {
+    // cerr << "Starting from " << beg << "," << end << endl;
+    // Backward search until first mismatch
+    range = index.charRange(index.alpha.char2comp[seq->seq.s[beg]]);
+    --beg;
+    while (beg >= 0 && gcsa::Range::length(range) > 0) {
+      // cerr << "Backward extending to " << beg << endl;
+      range = index.LF(range, index.alpha.char2comp[seq->seq.s[beg]]);
+      --beg;
+    }
+    if (beg < 0)
+      // no specific since we matched the entire query
+      continue;
+    // we must have a specific
+    ++beg;
+    // cerr << "Mismatch at " << beg << endl;
+
+    // Forward search until first mismatch
+    p = beg;
+    // cerr << "Starting from " << p << "," << end << endl;
+    range = index.charRange(index.alpha.char2comp[RCN[seq->seq.s[p]]]);
+    // range = rcindex.charRange(rcindex.alpha.char2comp[seq->seq.s[p]]);
+    ++p;
+    while (p <= end && gcsa::Range::length(range) > 0) {
+      // cerr << "Forward extending to " << p << endl;
+      range = index.LF(range, index.alpha.char2comp[RCN[seq->seq.s[p]]]);
+      // range = rcindex.LF(range, rcindex.alpha.char2comp[seq->seq.s[p]]);
+      ++p;
+    }
+    --p;
+    // cerr << "Mismatch at " << p << endl;
+    // cout << "N " << seq->name.s << " " << beg << " " << p << " "
+    //      << p - beg + 1 << endl;
+    specifics.push_back(
+        make_pair(max(0, beg - flank), min(p + flank, (int)(seq->seq.l - 1))));
+    beg = p - 1;
+    end = beg;
+    if (specifics.back().first == 0)
+      break;
+  }
+  cerr << "Assembling " << specifics.size() << " specific strings.." << endl;
+  vector<pair<int, int>> assembled_specifics;
+  assemble(specifics, assembled_specifics);
+  solutions = assembled_specifics;
+}
+
+void dump(kseq_t *seq, const vector<pair<int, int>> &solutions) {
+  cerr << "Dumping " << solutions.size() << " strings.." << endl;
+  for (const auto &spec : solutions) {
+    string S(seq->seq.s, spec.first, spec.second - spec.first + 1);
+    if (seq->qual.l == 0) {
+      cout << ">" << seq->name.s << ":" << spec.first << "-" << spec.second
+           << "\n"
+           << S << endl;
+    } else {
+      string Q(seq->qual.s, spec.first, spec.second - spec.first + 1);
+      cout << "@" << seq->name.s << ":" << spec.first << "-" << spec.second
+           << "\n"
+           << S << "\n"
+           << "+"
+           << "\n"
+           << Q << endl;
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
   parse_arguments(argc, argv);
 
@@ -42,115 +139,14 @@ int main(int argc, char *argv[]) {
   gcsa::GCSA index;
   index.load(ifs);
   ifs.close();
-  ifs.open(opt::index_path + ".lcp", std::ifstream::in);
-  gcsa::LCPArray lcp;
-  lcp.load(ifs);
-  ifs.close();
 
   gzFile fp = gzopen(opt::fx_path.c_str(), "r");
   kseq_t *seq = kseq_init(fp);
   int l;
-  gcsa::range_type range;
-  vector<gcsa::node_type> results;
-  int beg, end, p;
-  vector<pair<int, int>> specifics;
   while ((l = kseq_read(seq)) >= 0) {
-    cerr << "Querying " << seq->name.s << ".." << endl;
-    beg = l - 1;
-    end = beg;
-    while (beg >= 0) {
-      // cerr << "Starting from " << beg << "," << end << endl;
-      // Backward search until first mismatch
-      range = index.charRange(index.alpha.char2comp[seq->seq.s[beg]]);
-      --beg;
-      while (beg >= 0 && gcsa::Range::length(range) > 0) {
-        // cerr << "Backward extending to " << beg << endl;
-        range = index.LF(range, index.alpha.char2comp[seq->seq.s[beg]]);
-        --beg;
-      }
-      if (beg < 0)
-        // no specific since we matched the entire query
-        continue;
-      // we must have a specific
-      ++beg;
-      // cerr << "Mismatch at " << beg << endl;
-
-      // Forward search until first mismatch
-      p = beg;
-      // cerr << "Starting from " << p << "," << end << endl;
-      range = index.charRange(index.alpha.char2comp[RCN[seq->seq.s[p]]]);
-      // range = rcindex.charRange(rcindex.alpha.char2comp[seq->seq.s[p]]);
-      ++p;
-      while (p <= end && gcsa::Range::length(range) > 0) {
-        // cerr << "Forward extending to " << p << endl;
-        range = index.LF(range, index.alpha.char2comp[RCN[seq->seq.s[p]]]);
-        // range = rcindex.LF(range, rcindex.alpha.char2comp[seq->seq.s[p]]);
-        ++p;
-      }
-      --p;
-      // cerr << "Mismatch at " << p << endl;
-      // cout << "N " << seq->name.s << " " << beg << " " << p << " "
-      //      << p - beg + 1 << endl;
-      specifics.push_back(make_pair(beg, p));
-      beg = p - 1;
-      end = beg;
-    }
-
-    // assemble specifics
-    cerr << "Assembling (round 1).." << endl;
-    vector<pair<int, int>> assembled_specifics;
-    int i = specifics.size() - 1;
-    while (i >= 0) {
-      int j;
-      for (j = i - 1; j >= 0; --j) {
-        if (specifics[j + 1].second < specifics[j].first) {
-          // non-overlapping
-          assembled_specifics.push_back(make_pair(
-              max(0, specifics[i].first - opt::f),
-              min(specifics[j + 1].second + opt::f, (int)(seq->seq.l - 1))));
-          i = j;
-          break;
-        }
-      }
-      if (j < 0) {
-        assembled_specifics.push_back(
-            make_pair(max(0, specifics[i].first - opt::f),
-                      min(specifics[0].second + opt::f, (int)(seq->seq.l - 1))));
-        i = j;
-      }
-    }
-
-    reverse(assembled_specifics.begin(), assembled_specifics.end());
-    
-    // assemble assembled specifics (2nd round due to flanking)
-    cerr << "Assembling (round 2).." << endl;
-    vector<pair<uint, uint>> final_assembled_specifics;
-    i = assembled_specifics.size() - 1;
-    while (i >= 0) {
-      int j;
-      for (j = i - 1; j >= 0; --j) {
-        if (assembled_specifics[j + 1].second < assembled_specifics[j].first) {
-          // non-overlapping
-          final_assembled_specifics.push_back(make_pair(
-              assembled_specifics[i].first, assembled_specifics[j + 1].second));
-          i = j;
-          break;
-        }
-      }
-      if (j < 0) {
-        final_assembled_specifics.push_back(make_pair(
-            assembled_specifics[i].first, assembled_specifics[0].second));
-        i = j;
-      }
-    }
-
-    cerr << "Dumping.." << endl;
-    for (const auto &spec : final_assembled_specifics) {
-      string Q(seq->seq.s, spec.first, spec.second - spec.first + 1);
-      cout << ">" << seq->name.s << ":" << spec.first << "-" << spec.second
-           << "\n"
-           << Q << endl;
-    }
+    vector<pair<int, int>> solutions;
+    query(index, seq, opt::f, solutions);
+    dump(seq, solutions);
   }
 
   kseq_destroy(seq);
