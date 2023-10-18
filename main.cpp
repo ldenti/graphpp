3e9257fb5c682d20a19ed7af486f2ec4e1ba2e28
Luca Denti <ldenti@proton.me>
2023-10-18 21:22:23 +0200

    Add code to perform exact matching

--- main.cpp
+++ main.cpp
@@ -55,73 +55,24 @@ void assemble(const vector<pair<int, int>> &specifics,
 }
 
 void query(const gcsa::GCSA &index, const string &seq, int flank,
-           vector<pair<int, int>> &solutions) {
-  gcsa::range_type range;
+           vector<string> &solutions) {
+  if (seq.size() == 0 || seq.size() > 255)
+    // FIXME
+    return;
+  gcsa::range_type range = index.find(seq);
+  // if (gcsa::Range::empty(range))
+  //   return;
   vector<gcsa::node_type> results;
-  vector<pair<int, int>> specifics;
-  int seql = seq.size();
-  int beg = seql - 1;
-  int end = beg;
-  int p;
-  while (beg >= 0) {
-    // cerr << "Starting from " << beg << "," << end << endl;
-    // Backward search until first mismatch
-    range = index.charRange(index.alpha.char2comp[seq[beg]]);
-    --beg;
-    while (beg >= 0 && gcsa::Range::length(range) > 0) {
-      // cerr << "Backward extending to " << beg << endl;
-      range = index.LF(range, index.alpha.char2comp[seq[beg]]);
-      --beg;
-    }
-    if (beg < 0)
-      // no specific since we matched the entire query
-      continue;
-    // we must have a specific
-    ++beg;
-    // cerr << "Mismatch at " << beg << endl;
-
-    // Forward search until first mismatch
-    p = beg;
-    // cerr << "Starting from " << p << "," << end << endl;
-    range = index.charRange(index.alpha.char2comp[RCN[seq[p]]]);
-    // range = rcindex.charRange(rcindex.alpha.char2comp[seq->seq.s[p]]);
-    ++p;
-    while (p <= end && gcsa::Range::length(range) > 0) {
-      // cerr << "Forward extending to " << p << endl;
-      range = index.LF(range, index.alpha.char2comp[RCN[seq[p]]]);
-      // range = rcindex.LF(range, rcindex.alpha.char2comp[seq->seq.s[p]]);
-      ++p;
-    }
-    --p;
-    // cerr << "Mismatch at " << p << endl;
-    specifics.push_back(
-        make_pair(max(0, beg - flank), min(p + flank, seql - 1)));
-    beg = p - 1;
-    end = beg;
-    if (specifics.back().first == 0)
-      break;
-  }
-  vector<pair<int, int>> assembled_specifics;
-  assemble(specifics, assembled_specifics);
-  solutions = assembled_specifics;
+  index.locate(range, results);
+  cout << seq.size() << " " << results.size() << endl;
+  for (const auto &r : results)
+    solutions.push_back(gcsa::Node::decode(r));
 }
 
 void dump(const tuple<string, string, string> &input,
-          const vector<pair<int, int>> &output) {
-  for (const auto &o : output) {
-    string S(get<1>(input), o.first, o.second - o.first + 1);
-    if (get<2>(input).size() == 0) {
-      cout << ">" << get<0>(input) << ":" << o.first << "-" << o.second << "\n"
-           << S << endl;
-    } else {
-      string Q(get<2>(input), o.first, o.second - o.first + 1);
-      cout << "@" << get<0>(input) << ":" << o.first << "-" << o.second << "\n"
-           << S << "\n"
-           << "+"
-           << "\n"
-           << Q << endl;
-    }
-  }
+          const vector<string> &output) {
+  for (const auto &o : output)
+    cout << get<0>(input) << " " << o << endl;
 }
 
 bool load_batch(kseq_t *seq, vector<tuple<string, string, string>> &input) {
@@ -150,7 +101,7 @@ int main(int argc, char *argv[]) {
   cerr << " Done." << endl;
 
   vector<tuple<string, string, string>> input(opt::b);
-  vector<vector<pair<int, int>>> output(opt::b);
+  vector<vector<string>> output(opt::b);
 
   gzFile fp = gzopen(opt::fx_path.c_str(), "r");
   kseq_t *seq = kseq_init(fp);
@@ -164,14 +115,12 @@ int main(int argc, char *argv[]) {
     cerr << "Analyzing batch " << n << ".."
          << "\r";
 #pragma omp parallel for num_threads(opt::t)
-    for (i = 0; i < input.size(); ++i) {
+    for (i = 0; i < input.size(); ++i)
       query(index, get<1>(input[i]), opt::f, output[i]);
-    }
     cerr << "Dumping   batch " << n << ".."
          << "\r";
-    for (i = 0; i < input.size(); ++i) {
+    for (i = 0; i < input.size(); ++i)
       dump(input[i], output[i]);
-    }
     ++n;
   }
   cerr << "Dumped " << n - 1 << " batches. Done." << endl;
